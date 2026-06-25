36c5572d20001db184f80f371b912fcdc6e63fb2
Afzal Sheikh (fzryzen) <afzalex.store@gmail.com>
2026-06-26 03:01:39 +0530

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,4 +11,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  server: {
+    host: true,
+    allowedHosts: ["blog.afzalex.com", "localhost", "127.0.0.1", "::1"],
+    port: 4321,
+  },
 });
